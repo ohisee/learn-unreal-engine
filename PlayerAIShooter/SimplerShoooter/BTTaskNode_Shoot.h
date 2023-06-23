949ed71096be5da2e278fb68e5389e9ca55eee77
ohisee <breakosin@hotmail.com>
2023-06-23 00:15:18 -0700

    create a simple shooter game

---
+++ PlayerAIShooter/SimplerShoooter/BTTaskNode_Shoot.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTaskNode_Shoot.generated.h"
+
+/**
+ * This is behavior tree task class.
+ *
+ * Choose UBTTaskNode because we do not need the blackboard key selection list.
+ */
+UCLASS()
+class SIMPLERSHOOOTER_API UBTTaskNode_Shoot : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTaskNode_Shoot();
+
+protected:
+	// from UBTTaskNode.h
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory) override;
+};
