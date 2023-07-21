9de0ff1658493002bb57064f8e101f78d4dbb34f
ohisee <breakosin@hotmail.com>
2023-07-21 00:03:37 -0700

    a new game

---
+++ SlashMaximum/SlashMaximum/DebugMacros.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "DrawDebugHelpers.h"
+
+#define DRAW_SPHERE(Location) if (GetWorld() != nullptr) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+
+#define DRAW_SPHERE_SingleFrame(Location) if (GetWorld() != nullptr) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, false, -1.f);
+
+#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld() != nullptr) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
+
+#define DRAW_LINE_SingleFrame(StartLocation, EndLocation) if (GetWorld() != nullptr) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, -1.f, 0, 1.f);
+
+#define DRAW_POINT(Location) if (GetWorld() != nullptr) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Red, true);
+
+#define DRAW_POINT_SingleFrame(Location) if (GetWorld() != nullptr) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Red, false, -1.f);
+
+#define DRAW_VECTOR(StartLocation, EndLocation) if (GetWorld() != nullptr) \
+	{ \
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f); \
+		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Red, true); \
+	}
+
+#define DRAW_VECTOR_SingleFrame(StartLocation, EndLocation) if (GetWorld() != nullptr) \
+	{ \
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, -1.f, 0, 1.f); \
+		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Red, false, -1.f); \
+	}
+
+#define DRAW_SPHERE_COLOR(Location, Color) if (GetWorld() != nullptr) DrawDebugSphere(GetWorld(), Location, 25.f, 12, Color, true);
+
+#define DRAW_BOX(Location) if (GetWorld() != nullptr) DrawDebugBox(GetWorld(), Location, FVector(10.f), FColor::Red, true, -1.f, 0, 1.f);
